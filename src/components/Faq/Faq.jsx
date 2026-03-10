ac7b801c5a8dc08fd7ac569d738287ed13afafa4
Muzamil Hussain <3uzamilhussain@gmail.com>
2026-03-10 16:37:59 +0500

    init: upload files

---
+++ src/components/Faq/Faq.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { QuestionData } from "./QuestionData";
+let Faq = () => {
+  let [showAns, setShowAns] = useState(0);
+  let items = QuestionData.map((itemsData, i) => {
+    let faq = {
+      itemsData,
+      showAns,
+      setShowAns,
+    };
+    return <FaqItems faq={faq} key={i} />;
+  });
+  return (
+    <div>
+      <h1 className="faqHeading">FAQ</h1>
+      {items}
+    </div>
+  );
+}
+
+function FaqItems({faq}) {
+  let {itemsData,showAns,setShowAns} = faq;
+//   console.log(itemsData)
+  return (
+    <div className="faqItems">
+      <h2 onClick={() => setShowAns(showAns == itemsData.id ? 0 : itemsData.id )}>{itemsData.question}</h2>
+      <p className={showAns == itemsData.id ? "show" : ""}>
+        {itemsData.answer}
+      </p>
+    </div>
+  );
+}
+
+export default Faq;
