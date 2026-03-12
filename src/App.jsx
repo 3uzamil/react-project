d4a67cd4c441da394bddb4126313617878851794
Muzamil Hussain <3uzamilhussain@gmail.com>
2026-03-12 16:42:11 +0500

    feat: create todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Faq from './components/Faq/Faq'
+import TodoList from './components/TodoList/TodoList'
 
 export default function App() {
   return (
     <div>
-      <Faq />
+      {/* <Faq /> */}
+      <TodoList />
     </div>
   )
 }
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./TodoList.css";
+
+export default function TodoList() {
+  let [todoItems, setTodoItems] = useState([]);
+  let saveTodo = (event) => {
+    event.preventDefault();
+    let inputValue = event.target.todoInput;
+    if (inputValue.value != "") {
+      if (!todoItems.includes(inputValue.value)) {
+        let finalTodo = [...todoItems, inputValue.value];
+        setTodoItems(finalTodo);
+        inputValue.value = "";
+      } else {
+        alert("Todo already exist in list.");
+      }
+    }
+    else{
+        alert("Todo is empty");
+    }
+  };
+  let todoContext = todoItems.map((item, index) => {
+    return <TodoHandle todoItems={item} key={index} />;
+  });
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Todo List</h1>
+      <form onSubmit={saveTodo} className="todoForm">
+        <input type="text" name="todoInput" />
+        <button type="submit">Save</button>
+      </form>
+      <div className="contextCont">
+        <ul>{todoContext != undefined ? todoContext : ""}</ul>
+      </div>
+    </div>
+  );
+}
+
+function TodoHandle({ todoItems }) {
+  return <li>{todoItems}</li>;
+}
