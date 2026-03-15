05e35c2fcb8c86ac39eba83332fb23014f50c727
Muzamil Hussain <3uzamilhussain@gmail.com>
2026-03-15 14:44:39 +0500

    fix: set inputs on control state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 
 export default function Login() {
+  let [loginForm,setLoginForm] = useState({
+    email: "",
+    password: ""
+  });
+  let formHandle = (event) => {
+    let finalForm = {...loginForm};
+    let inputName = event.target.name;
+    let inputValue = event.target.value;
+    finalForm[inputName] = inputValue;
+    setLoginForm(finalForm);
+  };
   return (
     <>
     <Header pageName="Login" />
     <form className='loginForm'>
       <div className='formRow'>
         <label>Email</label>
-        <input type="email" />
+        <input type="email" onChange={formHandle} value={loginForm.email} name='email' required/>
       </div>
       <div className='formRow'>
         <label>Password</label>
-        <input type="text" />
+        <input type="text" onChange={formHandle} value={loginForm.password} name='password' required/>
       </div>
         <button type="submit">Login</button>
     </form>
