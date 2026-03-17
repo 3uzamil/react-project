4c7351e75d5924045b8f08c6f6b02b59d26cea83
Muzamil Hussain <3uzamilhussain@gmail.com>
2026-03-17 14:15:26 +0500

    fix: design registration page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,6 +5,21 @@ export default function Register() {
   return (
     <>
     <Header pageName={"Registeration Form"} />
+    <section className='registerationSection'>
+    <form className='form'>
+        <label>Name</label>
+        <input type="text" name='name' required/>
+        <label>Email</label>
+        <input type="text" name='email' required/>
+        <label>Phone</label>
+        <input type="text" name='phone' required/>
+        <label>Address</label>
+        <textarea type="text" name='address' required/>
+        <button type="submit">Save</button>
+    </form>
+    <div className='outputContainer'>
+    </div>
+    </section>
     </>
   )
 }
